9ff10fcd1094b0736f52dcadf741a487ea57f6f3
b-juu <erichassett4@gmail.com>
2023-10-17 10:17:47 -0400

    init commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
 {
-    public class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<string> videoGames = new List<string>
         {
-            Console.WriteLine("Hello, World!");
+            "The Legend of Zelda: Breath of the Wild",
+            "Red Dead Redemption 2",
+            "The Witcher 3: Wild Hunt",
+            "Counter-Strike: Global Offensive",
+            "Minecraft",
+            "Fortnite",
+            "Call of Duty: Warzone",
+            "Persona 5",
+            "Dark Souls III",
+            "Super Mario Odyssey"
+        };
+       
+        var orderedGames = videoGames.OrderBy(game => game.Length);
+
+        Console.WriteLine("List of video games ordered by name length:");
+        foreach (var game in orderedGames)
+        {
+            Console.WriteLine(game);
         }
+
+        string longestGameName = videoGames.OrderByDescending(game => game.Length).First();
+        Console.WriteLine($"Longest game name: {longestGameName}");
+
+        var shortNames = videoGames.Where(game => game.Length < 20);
+        Console.WriteLine("Games with names shorter than 20 characters:");
+        foreach (var game in shortNames)
+        {
+            Console.WriteLine(game);
+        }
+
+        int gamesWithThe = videoGames.Count(game => game.Contains("The"));
+        Console.WriteLine($"Number of games with 'The' in their name: {gamesWithThe}");
+
+        bool hasMinecraft = videoGames.Any(game => game.Contains("Minecraft"));
+        Console.WriteLine("Contains 'Minecraft': " + hasMinecraft);
     }
 }
